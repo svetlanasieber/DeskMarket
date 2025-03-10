e65f98262bb8f8ba76e1803aaf98ab4ae5cdd683
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 20:53:52 +0100

    Update Login.cshtml.cs

--- DeskMarket/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DeskMarket/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -93,7 +93,7 @@ namespace DeskMarket.Areas.Identity.Pages.Account
 
             returnUrl ??= Url.Content("~/");
 
-            // Clear the existing external cookie to ensure a clean login process
+         
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
@@ -109,8 +109,7 @@ namespace DeskMarket.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
@@ -133,7 +132,7 @@ namespace DeskMarket.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
+           
             return Page();
         }
     }
